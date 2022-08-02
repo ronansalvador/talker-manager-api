856da7c9474badf993ff5f3663effc040a4ab179
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-08-02 18:22:18 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND = 404;
 const PORT = '3000';
 
 const PATH = './talker.json';
@@ -16,8 +17,18 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (req, res) => {
   const talker = await fs.readFile(PATH, 'utf-8');
-  if (!talker) return res.status(200).json([]);
-  res.status(200).json(JSON.parse(talker));
+  if (!talker) return res.status(HTTP_OK_STATUS).json([]);
+  res.status(HTTP_OK_STATUS).json(JSON.parse(talker));
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talkers = await fs.readFile(PATH, 'utf-8');
+  const { id } = req.params;
+  const talker = JSON.parse(talkers).find((item) => item.id === Number(id));
+  if (!talker) {
+    return res.status(HTTP_NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {
